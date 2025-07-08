2f7e5ba248355f6cde567987593dbcc2935e76d1
philip.i <philip.i@ar-worx.com>
2025-07-08 12:31:19 +0100

    sort

---
+++ advanceSortArray.js
@@ -0,0 +1,40 @@
+// advance sort array of objects by multiple keys
+const array = [1, 2, 3, 4, 5, 6, 7, 100, 25]
+console.log(array) // main array
+
+// Sort numbers in ascending order
+const sortedAscending = array.sort((a, z) => a - z);
+
+// sort number in descending order
+const sortedDescending = array.sort((a, z) => z - a);
+
+const arrayOfPeople = [
+    {
+        name: 'Jane', age: 25, city: 'Los Angeles', community: {
+            name: 'T',
+            members: 1200
+        }
+    },
+    {
+        name: 'Mike', age: 30, city: 'Chicago', community: {
+            name: 'A',
+            members: 5700
+        },
+    },
+    {
+        name: 'Alice', age: 18, city: 'New York', community: {
+            name: 'B',
+            members: 1400
+        }
+    },
+    {
+        name: 'Bob', age: 22, city: 'Los Angeles', community: {
+            name: 'C',
+            members: 100
+        }
+    },
+
+];
+// Sort array of objects by age in ascending order
+const sortedByAgeAscending = arrayOfPeople.sort((a, b) => a.community.members - b.community.members);
+console.log(sortedByAgeAscending);
---
+++ sortArray.js
@@ -0,0 +1,18 @@
+// Basic sort array
+// what is sorting
+
+const arrayOfNumbers = [5, 3, 8, 1, 2];
+const arrayOfStrings = ['banana', 'apple', 'orange', 'grape'];
+
+
+// Sort numbers in ascending order
+console.log(arrayOfNumbers) //main array
+const sortedNumbers = arrayOfNumbers.sort();
+console.log(sortedNumbers); // Output: [1, 2, 3, 5, 8]
+
+
+// Sort strings in alphabetical order
+console.log(arrayOfStrings) //main array
+const sortedStrings = arrayOfStrings.sort();
+console.log(sortedStrings); // Output: ['apple', 'banana', 'grape', 'orange']
+
